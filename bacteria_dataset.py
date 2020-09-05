944baf00d448cd4ccaf8e0d2f4e21245132b2f3b
Jonathan-07 <57955969+Jonathan-07@users.noreply.github.com>
2020-09-05 16:43:51 +0100

    Update bacteria_dataset.py

--- bacteria_dataset.py
+++ bacteria_dataset.py
@@ -39,7 +39,7 @@ class Bacteria(Dataset):
         print('There are ', self.number_image, 'images')
         print('The size', self.image_width, ' and ', self.image_height)
 
-bacteria_data = Bacteria('../Data/20160712_SJ102_Pers_fos.nd2%20-%2020160712_SJ102_Pers_fos.nd2%20%28series%20001%29-2.tif')
+bacteria_data = Bacteria('home/john/Data/20160712_SJ102_Pers_fos.nd2%20-%2020160712_SJ102_Pers_fos.nd2%20%28series%20001%29-2.tif')
 phase, mask = bacteria_data.__getitem__(1)
 plt.imshow(phase)
 plt.show()

07626a21cbed37c75327ee68860ca8a70e8569a8
Jonathan-07 <57955969+Jonathan-07@users.noreply.github.com>
2020-10-21 12:44:11 +0100

    Update UNet_training.py

--- UNet_training.py
+++ UNet_training.py
@@ -96,7 +96,7 @@ def correct(outputs, mask):
 
 dataset = CarvanaDataset(dir_img, dir_mask, scale=0.5)
 validation_split = .99
-epochs = 10
+epochs = 5
 
 dataset_size = dataset.__len__()
 indices = list(range(dataset_size))
